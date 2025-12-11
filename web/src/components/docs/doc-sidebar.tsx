d6ff512b5bc4fc85dbd9320ed75a8ee437266a75
brandonkachen <brandonchenjiacheng@gmail.com>
2025-12-11 15:55:28 -0800

    docs: streamline documentation copy and rename Getting Started to Quick Start

--- web/src/components/docs/doc-sidebar.tsx
+++ web/src/components/docs/doc-sidebar.tsx
@@ -14,7 +14,7 @@ import { cn } from '@/lib/utils'
 
 const learnSections = [
   {
-    title: 'Getting Started',
+    title: 'Quick Start',
     href: '/docs/help',
     subsections: getDocsByCategory('help').map((doc) => ({
       title: doc.title,
